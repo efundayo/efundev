4c5069b8a9b04f718339b737205da8d1f051db7f
efundayo <efundayo@gmail.com>
2017-07-17 00:41:08 +0100

    Project update

--- main.sh
+++ main.sh
@@ -400,7 +400,7 @@ cat <<'EOF' > icinga2master_dbbackup.sh
  
 USER="root"
 PASSWORD="mysqlrootpassword"
-OUTPUT=" /root/backup/icinga2master"
+OUTPUT=/root/backup/icinga2master
  
 rm "$OUTPUT/*gz" > /dev/null 2>&1
  
@@ -427,7 +427,7 @@ cat <<'EOF' > dbserverbackup.sh
 DBHOST="dbserver.mosudi"
 USER="root"
 PASSWORD="mysqlrootpassword"
-OUTPUT="/root/backup/dbserver"
+OUTPUT=/root/backup/dbserver
  
 rm "$OUTPUT/*gz" > /dev/null 2>&1
  
@@ -450,16 +450,17 @@ cat <<'EOF' >s3backupscript.sh
 #!/bin/bash
 /root/backup/script/icinga2master_dbbackup.sh
 /root/backup/script/dbserverbackup.sh
-touch ~/old_perfdata.txt
 cd /var/spool/icinga2/perfdata/ ; ls  -d -1 $PWD/{*,*} > ~/new_perfdata.txt
 grep -v -F -x -f ~/old_perfdata.txt ~/new_perfdata.txt > ~/action_perfdata.txt
 #cat ~/action_perfdata.txt | xargs -I % aws s3 cp s3://mioemi2000/
-cd ~
-mkdir copytest
-cat ~/action_perfdata.txt | xargs -I % aws s3 cp ~/copytest/
-mv ~/new_perfdata.txt ~/old_perfdata.txt
-aws s3 cp -r /root/backup/  s3://mioemi2000/
+xargs -a ~/action_perfdata.txt aws s3 cp s3://mioemi2000/
 
+cd ~
+mkdir copytest > /dev/null 2>&1
+cat ~/action_perfdata.txt | xargs -I %  cp ~/copytest/
+mv ~/new_perfdata.txt ~/old_perfdata.txt
+#aws s3 cp -r /root/backup/  s3://mioemi2000/
+aws s3 cp -r /root/backup/  ~/copytest/
 
 EOF
 chmod +x s3backupscript.sh
--- s3backupscript.sh
+++ s3backupscript.sh
@@ -7,7 +7,7 @@ grep -v -F -x -f ~/old_perfdata.txt ~/new_perfdata.txt > ~/action_perfdata.txt
 #cat ~/action_perfdata.txt | xargs -I % aws s3 cp s3://mioemi2000/
 cd ~
 mkdir copytest
-cat ~/action_perfdata.txt | xargs -I % aws s3 cp ~/copytest/
+cat ~/action_perfdata.txt | xargs -I %  cp ~/copytest/
 mv ~/new_perfdata.txt ~/old_perfdata.txt
 aws s3 cp -r /root/backup/  s3://mioemi2000/
 
